04b7ee5ed8c2edfa7741e584baed487739ef5805
phuongfunny <vovanphuong101098@gmail.com>
2024-03-29 23:17:55 +0700

    feat: build base source

---
+++ src/types/customButtonProps.d.ts
@@ -0,0 +1,13 @@
+interface ICustomButtonProps {
+  // [_: string]: string;
+  colorBt?: string;
+  borderColorBt?: string;
+  backgroundColorBt?: string;
+  colorBtHover?: string;
+  borderColorBtHover?: string;
+  backgroundColorBtHover?: string;
+  colorBtPressed?: string;
+  borderColorBtPressed?: string;
+  backgroundColorBtPressed?: string;
+  backgroundColorBtDisElevated?: string;
+}
