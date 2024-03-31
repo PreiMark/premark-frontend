012475679176c82e7ecc0b8e676f91b9edf06ea9
simonnguyenlife <simonnguyenlife@gmail.com>
2024-03-31 21:21:01 +0700

    fix: fixed styled components css

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
+}
+
+module.exports = nextConfig
 
 export default nextConfig;
