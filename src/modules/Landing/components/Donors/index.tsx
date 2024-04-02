0c9828c745186390229d7043ffe85274ed4d9a98
funster <vovanphuong101098@gmail.com>
2024-04-02 23:47:52 +0700

    feat: responsive layout landing page

--- src/modules/Landing/components/Donors/index.tsx
+++ src/modules/Landing/components/Donors/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import styled from "styled-components";
 import { LINK_VIDEO_DONORS } from "@/constant";
-export interface IDonorsProps { }
+export interface IDonorsProps {}
 import donors from "@/assets/images/donors.png";
 import Image from "next/image";
 
@@ -13,10 +13,26 @@ export default function Donors(props: IDonorsProps) {
         <Title>Collaborate, Innovate</Title>
         <Title>Succeed: Bringing Ideas to Life</Title>
       </ContentRight>
-      <Image src={donors.src} width={1118} height={210} alt="donors_premark" />
-      <video style={{ borderRadius: "16px" }} width={491} height={532} src={LINK_VIDEO_DONORS} muted autoPlay loop />
+      <ImageDonors>
+        <Image
+          src={donors.src}
+          // width={1118}
+          // height={210}
+          alt="donors_premark"
+          layout="fill"
+        />
+      </ImageDonors>
+      <video
+        style={{ borderRadius: "16px" }}
+        width={471}
+        height={532}
+        src={LINK_VIDEO_DONORS}
+        muted
+        autoPlay
+        loop
+      />
     </Container>
-  )
+  );
 }
 
 const Container = styled.div`
@@ -26,14 +42,35 @@ const Container = styled.div`
   align-items: center;
   gap: 100px;
   margin-top: 40px;
-`
+`;
 const ContentRight = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
   gap: 16px;
-`
+`;
+
+const ImageDonors = styled.div`
+  position: relative;
+  width: 1118px;
+  height: 210px;
+  @media (max-width: 1024px) {
+    margin: auto;
+    width: 868px;
+    height: 250px;
+  }
+  @media (max-width: 768px) {
+    margin: auto;
+    width: 330px;
+    height: 150px;
+  }
+  @media (max-width: 420px) {
+    width: 350px;
+    height: 145px;
+  }
+`;
+
 const PreMark = styled.div`
   display: flex;
   justify-content: center;
@@ -42,15 +79,24 @@ const PreMark = styled.div`
   height: 34px;
   border-radius: 100px;
   padding: 8px 16px;
-  color: linear-gradient(272.65deg, #9747FF 26.52%, #08E5B2 100%);
+  color: linear-gradient(272.65deg, #9747ff 26.52%, #08e5b2 100%);
   text-transform: uppercase;
   font-size: 18px;
-  background: linear-gradient(272.65deg, #9747FF 26.52%, #08E5B2 100%, rgba(1, 2, 20, 1), rgba(190, 166, 255, 1));
-
-`
+  background: linear-gradient(
+    272.65deg,
+    #9747ff 26.52%,
+    #08e5b2 100%,
+    rgba(1, 2, 20, 1),
+    rgba(190, 166, 255, 1)
+  );
+`;
 const Title = styled.div`
   font-size: 45px;
   font-weight: 700;
   line-height: 54px;
-  color: #FFF;
-`
+  color: #fff;
+  align-items: center;
+  @media (max-width: 420px) {
+    font-size: 32px;
+  }
+`;
