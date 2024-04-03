08079354d5f2b45fa0d40d99f0a506d9d66d731e
funster <vovanphuong101098@gmail.com>
2024-04-03 21:45:37 +0700

    fix: fix layout mobile

---
+++ src/assets/icons/VectorOne.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+export default function IconCheck() {
+  return (
+    <svg
+      width={25}
+      height={17}
+      viewBox="0 0 25 17"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1 16.777V6.25285L7.45027 1.5H24.0852"
+        stroke="#9747FF"
+        strokeWidth={2}
+      />
+      <path
+        d="M1 16.777V6.25285L7.45027 1.5H24.0852"
+        stroke="url(#paint0_linear_0_461)"
+        strokeWidth={2}
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_0_461"
+          x1="24.0852"
+          y1="12.8163"
+          x2="0.324616"
+          y2="11.1541"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset="0.256678" stopColor="#9747FF" />
+          <stop offset={1} stopColor="#08E5B2" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
