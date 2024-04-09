b1187cdecd90d7ca372ad3fbf2b6595e9d63df84
funster <vovanphuong101098@gmail.com>
2024-04-09 22:18:38 +0700

    feat: changed config wallet and add balance

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,1 +1,16 @@
-const helper = {};
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import * as anchor from "@project-serum/anchor";
+import { SOL_NETWORK } from "@/config/env.config";
+
+export const solNetwork = ()=> {
+  switch (SOL_NETWORK) {
+    case 'mainnet':
+     return WalletAdapterNetwork.Mainnet  
+    case 'testnet':
+      return WalletAdapterNetwork.Testnet  
+    default:
+        return WalletAdapterNetwork.Devnet  
+  }
+}
+
+export  const endpoint = anchor.web3.clusterApiUrl(solNetwork());
