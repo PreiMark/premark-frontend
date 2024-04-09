5814d04cdefe559f97095858f2857971e3554de4
funster <vovanphuong101098@gmail.com>
2024-04-09 23:40:15 +0700

    fix: fixed flow buy/sell

--- src/modules/Offer/views/index.tsx
+++ src/modules/Offer/views/index.tsx
@@ -1,39 +1,36 @@
-'use client';
+"use client";
 
-import { Stack } from '@mui/material';
-import LeftPanel from '../components/LeftPanel';
-import RightPanel from '../components/RightPanel';
-import { useEffect, useState } from 'react';
-import axiosInstance from '@/config/axios.config';
-import { useSearchParams } from 'next/navigation';
+import { Stack } from "@mui/material";
+import LeftPanel from "../components/LeftPanel";
+import RightPanel from "../components/RightPanel";
+import { useEffect, useState } from "react";
+import axiosInstance from "@/config/axios.config";
+import { useSearchParams } from "next/navigation";
 
 export default function OfferPage() {
-	const searchParams = useSearchParams();
-	const [offerData, setOfferData] = useState<any>();
+  const searchParams = useSearchParams();
+  const [offerData, setOfferData] = useState<any>();
 
-	useEffect(() => {
-		const fetchOfferData = async () => {
-			try {
-				const id = searchParams.get('id');
-				const res = await axiosInstance.get(`/offer/${id}`);
-				if (res) {
-					setOfferData(res.data.data.data);
-				}
-			} catch (error) {
-				console.log(error);
-			}
-		};
+  useEffect(() => {
+    const fetchOfferData = async () => {
+      try {
+        const id = searchParams.get("id");
+        const res = await axiosInstance.get(`/offer/${id}`);
+        if (res) {
+          setOfferData(res.data.data.data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-		fetchOfferData();
-	}, [searchParams]);
+    fetchOfferData();
+  }, [searchParams]);
 
-	return (
-		<Stack
-			direction={{ xs: 'column', lg: 'row' }}
-			spacing={'32px'}
-		>
-			<LeftPanel offerData={offerData} />
-			<RightPanel offerData={offerData} />
-		</Stack>
-	);
+  return (
+    <Stack direction={{ xs: "column", lg: "row" }} spacing={"32px"}>
+      <LeftPanel offerData={offerData} />
+      <RightPanel offerData={offerData} />
+    </Stack>
+  );
 }
