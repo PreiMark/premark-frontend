c22ef019e6704c11f4e7faaf2321cf1fa2e59cc8
funster <vovanphuong101098@gmail.com>
2024-04-23 22:16:17 +0700

    fix: added link waitlist feedback and add trust wallet

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,150 +1,140 @@
-'use client';
-import Logo from '@/assets/icons/Logo';
-import * as React from 'react';
-import styled from 'styled-components';
-import Link from 'next/link';
-import { Button, Stack } from '@mui/material';
-import { IconList } from '@/assets/icons';
-import SidebarMobile from '../SidebarMobile';
-import { LIST_MENU } from '@/constant';
-import { useWalletModal } from '@solana/wallet-adapter-react-ui';
-import { useWalletMultiButton } from '@solana/wallet-adapter-base-ui';
+"use client";
+import Logo from "@/assets/icons/Logo";
+import * as React from "react";
+import styled from "styled-components";
+import Link from "next/link";
+import { Button, Stack } from "@mui/material";
+import { IconList } from "@/assets/icons";
+import SidebarMobile from "../SidebarMobile";
+import { LIST_MENU } from "@/constant";
+import { useWalletModal } from "@solana/wallet-adapter-react-ui";
+import { useWalletMultiButton } from "@solana/wallet-adapter-base-ui";
 
 export interface IHeaderProps {}
 
 export default function Header(props: IHeaderProps) {
-	const [showSidebar, setShowSidebar] = React.useState<boolean>(false);
+  const [showSidebar, setShowSidebar] = React.useState<boolean>(false);
 
-	const { setVisible: setModalVisible } = useWalletModal();
-	const {
-		buttonState,
-		onConnect,
-		onDisconnect,
-		publicKey,
-		walletIcon,
-		walletName,
-	} = useWalletMultiButton({
-		onSelectWallet() {
-			setModalVisible(true);
-		},
-	});
+  const { setVisible: setModalVisible } = useWalletModal();
+  const {
+    buttonState,
+    onConnect,
+    onDisconnect,
+    publicKey,
+    walletIcon,
+    walletName,
+  } = useWalletMultiButton({
+    onSelectWallet() {
+      setModalVisible(true);
+    },
+  });
 
-	const handleShowSidebar = () => {
-		setShowSidebar(!showSidebar);
-	};
-	const handleCloseSidebar = () => {
-		setShowSidebar(!showSidebar);
-	};
+  const handleShowSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
+  const handleCloseSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
 
-	return (
-		<Sectionheader>
-			<StackCustom direction={'row'}>
-			<Link href='/'>
-						<Logo />
-					</Link>
-				<MenuCustom>
-					
-					<Stack
-						direction={'row'}
-						sx={{ alignItems: 'center' }}
-						spacing={5}
-					>
-						{LIST_MENU.map((item, index) => (
-							<Link
-								href={item.url}
-								key={index}
-							>
-								<MenuItem key={index}>{item.title}</MenuItem>
-							</Link>
-						))}
-					</Stack>
-				</MenuCustom>
-				<ListButton onClick={handleShowSidebar}>
-					<IconList />
-				</ListButton>
-				<ButtonJoin onClick={() => setModalVisible(true)}>
-					Join waitlist
-				</ButtonJoin>
+  return (
+    <Sectionheader>
+      <StackCustom direction={"row"}>
+        <Link href="/">
+          <Logo />
+        </Link>
+        <MenuCustom>
+          <Stack direction={"row"} sx={{ alignItems: "center" }} spacing={5}>
+            {LIST_MENU.map((item, index) => (
+              <Link href={item.url} key={index}>
+                <MenuItem key={index}>{item.title}</MenuItem>
+              </Link>
+            ))}
+          </Stack>
+        </MenuCustom>
+        <ListButton onClick={handleShowSidebar}>
+          <IconList />
+        </ListButton>
+        <a href="https://forms.gle/STMArkZmoj1AH5zX7" target={"_blank"}>
+          <ButtonJoin>Join waitlist</ButtonJoin>
+        </a>
 
-				<SidebarMobile
-					handleClose={handleCloseSidebar}
-					showSidebar={showSidebar}
-				/>
-			</StackCustom>
-		</Sectionheader>
-	);
+        <SidebarMobile
+          handleClose={handleCloseSidebar}
+          showSidebar={showSidebar}
+        />
+      </StackCustom>
+    </Sectionheader>
+  );
 }
 
 const Sectionheader = styled.div`
-	display: flex;
-	@media (max-width: 768px) {
-		border: none;
-		
-	}
-	
-	border: 1px solid #B47AFF;
-	border-top: none;
-	border-bottom-left-radius: 30px;
-	border-bottom-right-radius: 30px;
+  display: flex;
+  @media (max-width: 768px) {
+    border: none;
+  }
 
-	justify-content: space-between;
-	padding: 24px;
-	@media (max-width: 768px) {
-		padding: 0;
-	}
-	max-width: 1440px;
-	margin: auto;
+  border: 1px solid #b47aff;
+  border-top: none;
+  border-bottom-left-radius: 30px;
+  border-bottom-right-radius: 30px;
+
+  justify-content: space-between;
+  padding: 24px;
+  @media (max-width: 768px) {
+    padding: 0;
+  }
+  max-width: 1440px;
+  margin: auto;
 `;
 const StackCustom = styled(Stack)`
-	justify-content: space-between;
-	align-items: center;
-	width: 100%;
-	@media (max-width: 768px) {
-		padding: 20px;
-	}
-	@media (max-width: 1024px) {
-		gap: 26px;
-		flex-wrap: wrap;
-	}
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  @media (max-width: 768px) {
+    padding: 20px;
+  }
+  @media (max-width: 1024px) {
+    gap: 26px;
+    flex-wrap: wrap;
+  }
 `;
 
 const MenuCustom = styled(Stack)`
-
-	flex-direction: row;
-	gap: 26px;
-	@media (max-width: 1024px) {
-		gap: 26px;
-		flex-wrap: wrap;
-	}
-	@media (max-width: 768px) {
-		display: none;
-	}
+  flex-direction: row;
+  gap: 26px;
+  @media (max-width: 1024px) {
+    gap: 26px;
+    flex-wrap: wrap;
+  }
+  @media (max-width: 768px) {
+    display: none;
+  }
 `;
 
 const MenuItem = styled(Button)<any>`
-	color: ${(props) => (props.active ? '#9945ff' : '#fff')};
-	text-transform: uppercase;
+  color: ${(props) => (props.active ? "#9945ff" : "#fff")};
+  text-transform: uppercase;
 `;
 
 const ButtonJoin = styled(Button)`
-	background-color: #9945ff;
-	color: #fff;
-	padding: 12px 24px;
-	border-radius: 8px;
-	border: none;
-	text-transform: capitalize;
-	height: 45px;
-	@media (max-width: 768px) {
-		display: none;
-	}
-	&:hover {
-		opacity: 0.5;
-		background-color: #9945ff !important;
-	}
+  background-color: #9945ff;
+  color: #fff;
+  padding: 12px 24px;
+  border-radius: 8px;
+  border: none;
+  text-transform: capitalize;
+  height: 45px;
+  @media (max-width: 768px) {
+    display: none;
+  }
+  &:hover {
+    opacity: 0.5;
+    background-color: #9945ff !important;
+  }
 `;
 
 const ListButton = styled(Button)`
-	@media (min-width: 768px) {
-		display: none;
-	}
+  @media (min-width: 768px) {
+    display: none;
+  }
 `;
--- src/modules/Dashboard/views/index.tsx
+++ src/modules/Dashboard/views/index.tsx
@@ -1,34 +1,19 @@
-import QuizPoint from '../components/QuizPoint';
-import MarketPoint from '../components/MarketPoint';
-import { Grid, Stack } from '@mui/material';
-import MarketList from '../components/MarketList';
+import QuizPoint from "../components/QuizPoint";
+import MarketPoint from "../components/MarketPoint";
+import { Grid, Stack } from "@mui/material";
+import MarketList from "../components/MarketList";
 export default function DashboardPage() {
-	return (
-		<Grid
-			container
-			width={'100%'}
-			spacing={'32px'}
-		>
-			<Grid
-				item
-				xs={12}
-				lg={8}
-			>
-				<MarketPoint />
-			</Grid>
-			<Grid
-				item
-				xs={12}
-				lg={4}
-			>
-				<QuizPoint />
-			</Grid>
-			<Grid
-				item
-				xs={12}
-			>
-				<MarketList />
-			</Grid>
-		</Grid>
-	);
+  return (
+    <Grid container width={"100%"} spacing={"32px"}>
+      <Grid item xs={12} lg={8}>
+        <MarketPoint />
+      </Grid>
+      <Grid item xs={12} lg={4}>
+        <QuizPoint />
+      </Grid>
+      <Grid item xs={12}>
+        <MarketList />
+      </Grid>
+    </Grid>
+  );
 }
